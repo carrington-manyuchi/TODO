b4a5c3900371093e1479c603d238405b81c0c6a1
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-04-04 12:27:47 +0200

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Manyuchi, Carrington C on 2025/04/04.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
